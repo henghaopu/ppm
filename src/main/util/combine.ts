c9ef7d94d4eb33da9b341e5b2cc7749dbb6f731f
Heng-Hao Pu <henghaopu@gmail.com>
2022-06-22 17:59:02 -0500

    docs: 📝 document my combination thought process using an example

--- src/main/util/combine.ts
+++ src/main/util/combine.ts
@@ -1,3 +1,31 @@
+/**
+ *  A B C D E   C(5, 2) = 5!/(3!·2!) = 10
+ *  A B         1
+ *  A   C       2
+ *  A     D     3
+ *  A       E   4
+ *    B C       5
+ *    B   D     6
+ *    B     E   7
+ *      C D     8
+ *      C   E   9
+ *        D E   10
+ *
+ *    Tree structure:
+ *
+ *    ([],[A,B,C,D,E])
+ *        ([A],[B,C,D,E])
+ *            ([A,B],[C,D,E])  ([A,C], [D,E])  ([A,D], [E], ([A,E], [])
+ *        ([B], [C,D,E])
+ *            ([B,C],[D,E])  ([B,D],[E])  ([B,E], [])
+ *        ([C], [D,E])
+ *            ([C,D],[E])  ([C,E],[])
+ *        ([D], [E])
+ *            ([D,E],[])
+ *        ([E], [])
+ *
+ */
+
 const combine = (nUniqueItems: unknown[], k: number): unknown[][] => {
   const output: unknown[][] = []
   // const nums = [...Array(n).fill(0).keys()].map((val) => val + 1)
