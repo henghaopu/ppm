9f89cc8ead2927048e3cba2353d97092c1e54b2b
Heng-Hao Pu <henghaopu@gmail.com>
2022-07-19 22:14:16 -0500

    chore: :construction: prepare the heap skeleton

--- src/main/util/heap.ts
+++ src/main/util/heap.ts
@@ -1,4 +1,71 @@
+/* eslint-disable no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-useless-constructor */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-class Heap<T> {}
+export type Compare<T> = (a: T, b: T) => number
+
+class Heap<T> {
+  private data: T[] = []
+
+  constructor(private compare: Compare<T>) {}
+
+  private leftOf(nodeIndex: number): number {
+    return -1
+  }
+
+  private rightOf(nodeIndex: number): number {
+    return -1
+  }
+
+  private parentOf(nodeIndex: number): number {
+    return -1
+  }
+
+  /**
+   * Add the given item to the heap in O(logn)
+   * @param item
+   */
+  insert(item: T): void {
+    return undefined
+  }
+
+  /**
+   * Sift the node at the given index up to its proper index in the heap
+   * @param index
+   */
+  private siftUpFrom(index: number): void {
+    return undefined
+  }
+
+  /**
+   * extract the min/max value
+   */
+  extract(): T | undefined {
+    return undefined
+  }
+
+  /**
+   * Sift the node at the given index down to its proper index in the heap
+   * @param index
+   */
+  private siftDownFrom(index: number): void {
+    return undefined
+  }
+
+  /**
+   * return the number of items in the heap
+   */
+  size(): number {
+    return -1
+  }
+
+  /**
+   * return true if the heap is empty, false otherwise
+   */
+  isEmpty(): boolean {
+    return false
+  }
+}
 
 export default Heap
