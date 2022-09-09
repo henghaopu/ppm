98a5767a776fd6002e7252b47520532e1ed823d2
Heng-Hao Pu <henghaopu@gmail.com>
2022-09-09 18:50:56 -0500

    chore: clean up code; update homepage route

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -5,52 +5,43 @@ import raw from 'raw.macro'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-// import MinHeightDemo from './screen/MinHeightDemo'
-
 function App() {
   return (
     <div data-testid="app">
-      {/* <MinHeightDemo /> */}
-      <div style={{ height: '100vh', overflow: 'auto' }}>
-        {/* documentation: https://github.com/remarkjs/react-markdown */}
-        <ReactMarkdown
-          // change things like <code> that come from markdown
-          components={{
-            code: ({ inline, className, children, ...props }) => {
-              // className: set to language-js or so when using ```js
-              const match = /language-(\w+)/.exec(className || '')
+      {/* documentation: https://github.com/remarkjs/react-markdown */}
+      <ReactMarkdown
+        // change things like <code> that come from markdown
+        components={{
+          pre: ({ children }) => <pre className="">{children}</pre>,
+          code: ({ inline, className, children, ...props }) => {
+            // className: set to language-js or so when using ```js
+            const match = /language-(\w+)/.exec(className || '')
 
-              return !inline && match ? (
-                <SyntaxHighlighter
-                  {...props}
-                  // typescript, jsx, ...
-                  language={match[1]}
-                  style={vscDarkPlus}
-                  PreTag="div"
-                  showLineNumbers
-                >
-                  {/* codeString */}
-                  {String(children)}
-                </SyntaxHighlighter>
-              ) : (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              )
-            },
-          }}
-        >
-          {/* children: string - markdown to parse */}
-          {`~~~typescript\n${raw('./util/permute.ts')}\n~~~`}
-
-          {/* test another files */}
-          {/* {`~~~jsx\n${raw('./App.tsx')}\n~~~`} */}
-          {/* {`~~~typescript\n${raw('../test/util/permute.test.ts')}\n~~~`} */}
-
-          {/* maintain a .md file - against Single Source of Truth */}
-          {/* {raw('./markdown/permute.md')} */}
-        </ReactMarkdown>
-      </div>
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                // typescript, jsx, ...
+                language={match[1]}
+                style={vscDarkPlus}
+                PreTag="div"
+                showLineNumbers
+                // overwrite default margin-y=0.5em
+                customStyle={{ margin: 0 }}
+              >
+                {/* codeString */}
+                {String(children)}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          },
+        }}
+      >
+        {/* children: string - markdown to parse */}
+        {`~~~typescript\n${raw('./util/permute.ts')}\n~~~`}
+      </ReactMarkdown>
     </div>
   )
 }
