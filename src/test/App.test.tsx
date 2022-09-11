fd70e8b228bbcbd71de69f9b37a24b3c3b8dd4a5
Heng-Hao Pu <henghaopu@gmail.com>
2022-09-10 20:52:05 -0500

    test: :clown_face: mock window.matchMedia

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -2,6 +2,23 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from 'main/App'
 
+beforeEach(() => {
+  // Mocking methods which are not implemented in JSDOM. Reference: https://stackoverflow.com/questions/39830580/jest-test-fails-typeerror-window-matchmedia-is-not-a-function
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  })
+})
+
 test('renders learn react link', () => {
   render(<App />)
   const divElement = screen.getByTestId('app')
