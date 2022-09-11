1a613fb841d91fc083f6f216b030d4f6542d3d5b
Heng-Hao Pu <henghaopu@gmail.com>
2022-09-11 13:44:27 -0500

    feat: add prism code themes (#11)

--- src/main/component/Drawer.tsx
+++ src/main/component/Drawer.tsx
@@ -1,10 +1,34 @@
+import React from 'react'
+import * as prismThemes from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+type DrawerProps = {
+  utilFileNames: string[]
+  defaultCodeTheme: string
+  setCodeTheme: React.Dispatch<React.SetStateAction<string>>
+}
+
 // todo: support mobile
-function Drawer({ utilFileNames }: { utilFileNames: string[] }) {
+function Drawer({
+  utilFileNames,
+  defaultCodeTheme,
+  setCodeTheme,
+}: DrawerProps) {
   return (
     <div className="bg-base-200">
       <ul className="menu p-4 overflow-y-auto w-80 text-base-content h-full">
-        <li className="menu-title">
+        <li className="menu-title flex flex-row flex-nowrap items-center">
           <span>Programming Notes</span>
+          <select
+            className="select select-bordered select-xs w-20 grow m-1"
+            defaultValue={defaultCodeTheme}
+            onChange={(e) => setCodeTheme(e.target.value)}
+          >
+            {Object.keys(prismThemes).map((codeTheme) => (
+              <option key={codeTheme} value={codeTheme}>
+                {codeTheme}
+              </option>
+            ))}
+          </select>
         </li>
         {utilFileNames.map((fileName) => (
           <li key={fileName} className="hover-bordered">
